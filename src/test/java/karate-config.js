59fe5975f49271fe36aadff663d4bd9edc7ddd02
rameshthehope <48827816+rameshthehope@users.noreply.github.com>
2019-07-29 23:03:45 +0530

    Updated url.

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -2,6 +2,6 @@ function() {
 	var config = {
 			
 		  }
-	config.baseUrl = 'https://13.235.105.183:8080/Demo_HCS/calculator';
+	config.baseUrl = 'http://13.235.105.183:8080/Demo_HCS/calculator';
 	return config;
 }
