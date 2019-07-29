6b485517c1639174c5d296ae6dea070d5d038ed7
Pranesh Gaikwad <Pranesh.Gaikwad@harman.com>
2019-07-29 15:01:20 +0530

    Advanced Cucumber reporting
    Advanced Cucumber reporting

--- src/test/java/services/math/operations/MathsOperationsRunner.java
+++ src/test/java/services/math/operations/MathsOperationsRunner.java
@@ -1,10 +1,39 @@
 package services.math.operations;
 
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-import com.intuit.karate.junit4.Karate;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-@RunWith(Karate.class)
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+
+@KarateOptions(tags = {"~@ignore"})
 public class MathsOperationsRunner {
+	
+	 @Test
+	    public void testParallel() {
+	        Results results = Runner.parallel(getClass(), 5);	// Runs in 5 threads
+	        generateReport(results.getReportDir());
+	        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);        
+	    }
+	    
+	    public static void generateReport(String karateOutputPath) {        
+	        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+	        List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
+	        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+	        Configuration config = new Configuration(new File("target"), "Execution Tests");
+	        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+	        reportBuilder.generateReports();        
+	    }
 
 }
