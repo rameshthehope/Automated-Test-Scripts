f7fbe388f24aba80b1904489d6f3cbc8c61a8bcb
rameshthehope <48827816+rameshthehope@users.noreply.github.com>
2019-07-29 22:38:13 +0530

    Updated url to point to Dev environment.

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -2,6 +2,6 @@ function() {
 	var config = {
 			
 		  }
-	config.baseUrl = 'https://demohcs.cfapps.io/calculator';
+	config.baseUrl = 'https://13.235.105.183:8080/Demo_HCS/calculator';
 	return config;
 }
