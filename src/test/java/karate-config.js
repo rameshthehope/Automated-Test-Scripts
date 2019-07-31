69c9df0b7c81358e1eae5ec38b2644c839664122
rameshthehope <48827816+rameshthehope@users.noreply.github.com>
2019-07-31 13:17:27 +0530

    Added karate timeout.

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -3,5 +3,8 @@ function() {
 			
 		  }
 	config.baseUrl = 'http://13.235.105.183:8080/Demo_HCS/calculator';
+	// Timeout if a connection or servers don't respond within 5 seconds
+      	karate.configure('connectTimeout', 40000);
+      	karate.configure('readTimeout', 40000);
 	return config;
 }
