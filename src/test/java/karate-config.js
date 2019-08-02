beecad3686174a85a2c2501a29988c99c1b120ba
Pranesh Gaikwad <Pranesh.Gaikwad@harman.com>
2019-08-02 16:51:59 +0530

    Changes to handle multi environments
    Changes to handle multi environments

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,8 +1,21 @@
 function() {
+	
+	var env = karate.env; // Get system property 'karate.env'
+	  if (!env) {	// Set to this if no env provided
+	    env = 'DEV';
+	  }
+
+	  karate.log('Execution Environment is :', env);
 	var config = {
 			
 		  }
-	config.baseUrl = 'http://13.235.105.183:8080/Demo_HCS/calculator';
+	if (env == 'DEV') {
+		config.baseUrl = 'http://13.235.105.183:8080/Demo_HCS/calculator';
+		
+	} else if (env == 'UAT') {
+		config.baseUrl = 'https://demohcs.cfapps.io/calculator';
+		
+	}
 	// Timeout if a connection or servers don't respond within 5 seconds
       	karate.configure('connectTimeout', 40000);
       	karate.configure('readTimeout', 40000);
